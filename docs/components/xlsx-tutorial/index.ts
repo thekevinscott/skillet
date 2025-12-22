f7e2b53f78a97a20a7bec4ce107ec5e56a9343e2
clankerbot <248217931+clankerbot@users.noreply.github.com>
2025-12-22 18:28:58 +0000

    chore: rename component file to bust cache

--- docs/components/xlsx-tutorial/index.ts
+++ docs/components/xlsx-tutorial/index.ts
@@ -1,4 +1,4 @@
-import { SkilletXlsxTutorial, TAG_NAME } from './xlsx-tutorial.js'
+import { SkilletXlsxTutorial, TAG_NAME } from './xlsx-tutorial-v2.js'
 
 if (!customElements.get(TAG_NAME)) {
   customElements.define(TAG_NAME, SkilletXlsxTutorial)
